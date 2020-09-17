5a60e9f704e6aca57ab4264caf6dbc1f42a30671
Shir <shirerezrein@gmail.com>
2020-09-17 15:55:25 +1000

    set up app for heroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const { response } = require('express')
 const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+
 const app = express() 
 const port = process.env.PORT || 3000
 
